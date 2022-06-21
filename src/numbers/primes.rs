2acdb4e05ba12565939755b13fb1ca08a4d23bdf
Denis Tsvikevich <dtsvikevich@gmail.com>
2022-06-21 22:23:57 +0300

    fix: added check for 0 and 1 as exceptions
    Zero and one are not prime numbers, so previous version of functions doesn't work properly.

--- src/numbers/primes.rs
+++ src/numbers/primes.rs
@@ -8,6 +8,9 @@
 pub fn generate(upto: usize) -> Vec<usize> {
     let mut prime_flags = vec![true; upto];
 
+    // One is not a prime number.
+    prime_flags[0] = false;
+
     let mut current_smallest_prime = 2usize;
     while current_smallest_prime <= upto {
         if prime_flags[current_smallest_prime - 1] {
@@ -37,6 +40,10 @@ pub fn generate(upto: usize) -> Vec<usize> {
 ///
 /// Note: This function uses trial division.
 pub fn is_prime_trial(n: u64) -> bool {
+    if n <= 1 {
+        return false;
+    }
+
     let square_root = (n as f64).sqrt().trunc() as u64;
     for i in 2..=square_root {
         if n % i == 0 {
@@ -54,18 +61,31 @@ mod tests {
     #[test]
     fn primes_upto_thirty_should_be_generated_correctly() {
         let primes_upto_thirty = generate(30);
-        assert_eq!(primes_upto_thirty, vec![1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+        assert_eq!(primes_upto_thirty, vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
     }
 
     #[test]
     fn all_is_prime() {
-        let primes = vec![1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+        let primes = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
         assert!(primes.into_iter().all(|x| is_prime_trial(x)));
     }
 
     #[test]
     fn all_is_not_prime() {
-        let primes = vec![4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22];
+        let primes = vec![1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22];
         assert!(primes.into_iter().all(|x| !is_prime_trial(x)));
+    }
+
+    #[test]
+    fn zero_is_not_prime() {
+        let zero_is_prime = is_prime_trial(0);
+        assert_eq!(zero_is_prime, false);
+    }
+
+
+    #[test]
+    fn one_is_not_prime() {
+        let zero_is_prime = is_prime_trial(1);
+        assert_eq!(zero_is_prime, false);
     }
 }
